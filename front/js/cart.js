f88e2b8b766e40f4e0c2df74d82896cc239ecd5d
Hugo Lemerle <hlml200294@gmail.com>
2022-02-14 23:49:36 +0100

    First work on form cart page

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,6 +4,7 @@ function main()
 {
     const cart = getCart();
     displayCartProducts(cart);
+    formVerification()
 }
 
 // Get cart by parsing the item 'myCart' stored in localStorage
@@ -31,6 +32,27 @@ async function displayCartProducts(cart)
     let section = document.getElementById('cart__items');
     let numTotalQuantity = 0;
     let numTotalPrice = 0;
+
+    if (cart.length == 0) {
+        let kanapsLink = document.createElement('a');
+        kanapsLink.href = "index.html";
+        kanapsLink.textContent = "Voir nos produits";
+        kanapsLink.style.color = "white";
+        
+        let emptyCart = document.createElement('h3');
+        emptyCart.textContent = "Votre panier est vide ! ";
+        emptyCart.style.textAlign = "center";
+        emptyCart.appendChild(kanapsLink);
+        
+        section.appendChild(emptyCart);
+
+        let totalQuantity = document.getElementById('totalQuantity');
+        numTotalQuantity = 0;
+        totalQuantity.textContent = numTotalQuantity;
+        let totalPrice = document.getElementById('totalPrice');
+        numTotalPrice = 0;
+        totalPrice.textContent = numTotalPrice;
+    }
     
     // Creation of all HTML elements with a loop (looping into the cart)
     for (let i = 0; i < cart.length; i++) {
@@ -93,6 +115,9 @@ async function displayCartProducts(cart)
         {
             pQuantity.textContent = "Qté : " + this.value;
             cart[i].quantity = parseInt(this.value);
+            if (cart[i].quantity == 0) {
+                cart[i].quantity += 1;
+            }
             localStorage.setItem('myCart', JSON.stringify(cart));
             location.reload();
         });
@@ -127,13 +152,74 @@ async function displayCartProducts(cart)
         divCartItemContentSettingsDelete.appendChild(pDelete);
 
         // Diplay the total quantity of kanaps in cart
-        let totalQuantity = document.getElementById('totalQuantity');
         numTotalQuantity += cart[i].quantity;
         totalQuantity.textContent = numTotalQuantity;
         
-        // Display the total price of the order
-        let totalPrice = document.getElementById('totalPrice');
+        // Display the total price of the order 
         numTotalPrice += kanap.price * cart[i].quantity;
         totalPrice.textContent = numTotalPrice;
+    }
+}
+
+function formVerification()
+{
+    let firstName = document.getElementById('firstName');
+    let lastName = document.getElementById('lastName');
+    let address = document.getElementById('address');
+    let city = document.getElementById('city');
+    let email = document.getElementById('email');
+    
+    let firstNameErrorMsg = document.getElementById('firstNameErrorMsg');
+    let lastNameErrorMsg = document.getElementById('lastNameErrorMsg');
+    let addressErrorMsg = document.getElementById('addressErrorMsg');
+    let cityErrorMsg = document.getElementById('cityErrorMsg');
+    let emailErrorMsg = document.getElementById('emailErrorMsg');
+    
+    let regExLetters = /[A-Za-zÀ-ÖØ-öø-ÿ-'\s]/;
+    let regExLettersAndNumbers = /[A-Za-zÀ-ÖØ-öø-ÿ0-9-'\s]/;
+    
+    let order = document.getElementById('order');
+    
+    firstName.addEventListener('change', function() {
+        spacesGestion(firstName);
+        checkInput(firstName, firstNameErrorMsg, regExLetters, "Ce champ ne doit contenir que des lettres");
+    });
+    lastName.addEventListener('change', function() {
+        spacesGestion(lastName);
+        checkInput(lastName, lastNameErrorMsg, regExLetters, "Ce champ ne doit contenir que des lettres");
+    });
+    address.addEventListener('change', function() {
+        spacesGestion(address);
+        checkInput(address, addressErrorMsg, regExLettersAndNumbers, "Ce champ ne doit contenir que des lettres ou des chiffres");
+    });
+    city.addEventListener('change', function() {
+        spacesGestion(city);
+        checkInput(city, cityErrorMsg, regExLetters, "Ce champ ne doit contenir que des lettres");
+    });
+}
+
+function spacesGestion(stringInput)
+{
+    let stringModicated = stringInput.value.trim().split(/[\s,\t,\n]+/).join(" ");
+    stringInput.value = stringModicated;
+}
+
+function checkInput(input, inputErrorMsg, regEx, errorMsg)
+{
+    let verificationArray = [];
+    for (let i = 0; i < input.value.length; i++) {
+        let test = regEx.test(input.value[i]);
+        if (test == false) {
+            verificationArray.push(test);
+        }
+        else {
+            verificationArray.push(test);
+        }
+    }
+    if (verificationArray.includes(false)) {
+        inputErrorMsg.textContent = errorMsg;
+    }
+    else {
+        inputErrorMsg.textContent = "";
     }
 }
